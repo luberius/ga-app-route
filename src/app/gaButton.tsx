c29b5b2e131217c9b01f206c1c6161fd41c90fe0
syahril <callmesyahril@gmail.com>
2024-10-09 11:54:56 +0700

    init

---
+++ src/app/gaButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { sendGTMEvent } from "@next/third-parties/google";
+import Image from "next/image";
+
+const GaButton = () => {
+  const handleClick = () => {
+    sendGTMEvent({ event: "buttonClicked", value: "xyz" });
+  };
+
+  return (
+    <button
+      className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+      onClick={handleClick}
+      rel="noopener noreferrer"
+    >
+      <Image
+        className="dark:invert"
+        src="https://nextjs.org/icons/vercel.svg"
+        alt="Vercel logomark"
+        width={20}
+        height={20}
+      />
+      Test GA
+    </button>
+  );
+};
+
+export default GaButton;
